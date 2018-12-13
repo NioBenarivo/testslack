78f9f02670769e8fe014f39127817d4a13265bea
zunio <zunibenarivo@gmail.com>
2018-12-13 16:39:49 +0700

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var SlackBot = require("slackbots")
 var request = require("request")
-var endpoint = "https://icanhazdadjoke.com/slack"
+// var endpoint = "https://icanhazdadjoke.com/slack"
 
 const envKey = process.env.JOKES_BOT_TOKEN
 
@@ -10,29 +10,33 @@ var bot = new SlackBot({
   name: "Jokes Bot"
 })
 
-bot.on("message", msg => {
-  switch (msg.type) {
-    case "message":
-      // we only want to listen to direct messages that come from the user
-      if (msg.channel[0] === "D" && msg.bot_id === undefined) {
-        getRandomJoke(postMessage, msg.user)
-      }
-      break
-  }
-})
+bot.on("start", function() {
+  bot.postMessageToChannel(channel, "Hi, I'm alive and well! Please ask me anything related to unify! type `help` for more info");
+});
 
-const postMessage = (message, user) => {
-  bot.postMessage(user, message, { as_user: true })
-}
+// bot.on("message", msg => {
+//   switch (msg.type) {
+//     case "message":
+//       // we only want to listen to direct messages that come from the user
+//       if (msg.channel[0] === "D" && msg.bot_id === undefined) {
+//         getRandomJoke(postMessage, msg.user)
+//       }
+//       break
+//   }
+// })
 
-const getRandomJoke = (callback, user) => {
-  return request(endpoint, (error, response) => {
-    if (error) {
-      console.log("Error: ", error)
-    } else {
-      let jokeJSON = JSON.parse(response.body)
-      let joke = jokeJSON.attachments[0].text
-      return callback(joke, user)
-    }
-  })
-}
+// const postMessage = (message, user) => {
+//   bot.postMessage(user, message, { as_user: true })
+// }
+
+// const getRandomJoke = (callback, user) => {
+//   return request(endpoint, (error, response) => {
+//     if (error) {
+//       console.log("Error: ", error)
+//     } else {
+//       let jokeJSON = JSON.parse(response.body)
+//       let joke = jokeJSON.attachments[0].text
+//       return callback(joke, user)
+//     }
+//   })
+// }
